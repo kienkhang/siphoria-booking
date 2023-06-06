60fc761047ecaa7bbb90d458df6b414d93179778
KienKhang <trankienkhang2210@gmail.com>
2023-06-06 23:56:12 +0700

    feat: decrease overlay

--- components/base/Modal.vue
+++ components/base/Modal.vue
@@ -2,18 +2,18 @@
 teleport(to='body')
   div(v-auto-animate)
     .modal-provider.fixed.inset-0.flex.items-center.justify-center(v-if="show" class='z-[9999]')
-      .absolute.inset-0(class='bg-black/50 backdrop-blur-1px mask-overlay dark:bg-white/50' @click='$emit("mask-click")')
+      .absolute.inset-0(class='bg-black/25 backdrop-blur-1px mask-overlay dark:bg-white/50' @click='$emit("mask-click")')
       .relative(class='z-50 body-modal')
         slot
 </template>
-  
+
 <script setup lang="ts">
 // Props
 defineProps({
   show: {
     type: Boolean,
-    default: false,
-  },
+    default: false
+  }
 })
 // Emits
 defineEmits<{
