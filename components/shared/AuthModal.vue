9f46966d1d8c01e0e37293dd296bd13a1fef367a
KienKhang <trankienkhang2210@gmail.com>
2023-06-06 23:56:47 +0700

    feat: done modal

--- components/shared/AuthModal.vue
+++ components/shared/AuthModal.vue
@@ -1,20 +1,23 @@
 <template lang="pug">
 BaseModal(:show='show' @mask-click='closeModal')
-  .bg-white.w-500px.rounded-lg.px-5.py-6
+  .bg-white.w-400px.rounded-lg.px-5.py-6
+    //- Login Tab 
     .tabs-list.w-full.flex.items-center.justify-center
       .tab-items.flex.items-center.justify-center.h-9.cursor-pointer(class='w-40' :class='{"shadow-[0px_2px_#FF9900]":tab==="login"}' @click='changeTab("login")')
         span(:class='[{"font-semibold text-vivid":tab==="login"},{"text-gray":tab!=="login"}]') Login
       .tab-items.flex.items-center.justify-center.h-9.cursor-pointer(class='w-40' :class='{"shadow-[0px_2px_#FF9900]":tab==="signup"}' @click='changeTab("signup")')
         span(:class='[{"font-semibold text-vivid":tab==="signup"},{"text-gray":tab!=="signup"}]') Signup
-    .tab-panels.mt-2
-      .panel-item(v-show='tab==="login"' v-auto-animate)
+    .tab-panels.mt-4(v-auto-animate)
+      LoginSocial
+      .panel-item(v-if='tab==="login"')
         LoginForm
-      .panel-item(v-show='tab==="signup"' v-auto-animate)
+      .panel-item(v-else)
         SignupForm
 
 </template>
 
 <script setup lang="ts">
+import LoginSocial from './LoginSocial.vue';
 import LoginForm from './form/LoginForm.vue';
 import SignupForm from './form/SignupForm.vue';
 
