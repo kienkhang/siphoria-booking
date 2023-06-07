e38a4536d3b09bd235f1981d8746610b01bcce89
KienKhang <trankienkhang2210@gmail.com>
2023-06-07 23:09:28 +0700

    feat: call login social api

--- components/auth/LoginSocial.vue
+++ components/auth/LoginSocial.vue
@@ -1,13 +1,23 @@
 <template lang="pug">
 .grid.grid-cols-2.gap-2.px-4
-  BaseButton.justify-center.border.border-gray-400.py-2
+  BaseButton.justify-center.border.border-gray-400.py-2(@click='callGg()')
     .i-custom-google.w-4.h-4.mr-2.flex-shrink-0
     span.font-medium.text-gray-600 Google
-  BaseButton.justify-center.border.border-gray-400.py-2
+  BaseButton.justify-center.border.border-gray-400.py-2(@click='callFb()')
     .i-custom-facebook.w-4.h-4.mr-2.flex-shrink-0
     span.font-medium.text-gray-600 Facebook
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { authApi } from '~/apis/auth'
+
+const { execute: callGg, data: ggData } = authApi.loginGG()
+const { execute: callFb, data: fbData } = authApi.loginFB()
+
+watch([ggData, fbData], () => {
+  console.log('😃😦😧 ~ watch ~ fbData:', fbData.value)
+  console.log('😃😦😧 ~ watch ~ ggData:', ggData.value)
+})
+</script>
 
 <style scoped></style>
--- components/base/Button.vue
+++ components/base/Button.vue
@@ -1,8 +1,12 @@
 <template lang="pug">
-button.flex-inline.items-center.rounded-lg.transition-all(class='active:focus:scale-95 hover:bg-opacity-90')
+button.flex-inline.items-center.rounded-lg.transition-all(class='active:focus:scale-95 hover:bg-opacity-90' @click='$emit("click")' )
   slot
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineEmits<{
+  (e: 'click'): any
+}>()
+</script>
 
 <style scoped></style>
