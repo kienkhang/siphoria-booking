eb245cf9add605ddf83d6a16cefe64b788e53487
kienkhang <trankienkhang2210@gmail.com>
2023-06-07 18:23:41 +0700

    feat: migrate from shared to auth

--- components/auth/AuthButton.vue
+++ components/auth/AuthButton.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 BaseButton.px-4.py-2.border.border-white.text-white(class='hover:bg-white hover:text-green-500' )
   span(@click='show = true') Đăng nhập / Đăng ký
-SharedAuthModal(v-model:show='show')
+AuthModal(v-model:show='show')
 </template>
 
 <script setup lang="ts">
--- components/auth/form/LoginForm.vue
+++ components/auth/form/LoginForm.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 FormKit(type='form' v-model:model-value="loginData" name='login_form' id='login_form' @submit="doLogin()" :actions='false' style='width: 100%; padding: 16px;')
   .flex.flex-col.gap-2
-    FormKit(type='text' label='Email' name="email" placeholder='Nhập email của bạn' validation="required|email" input-class='transition-all border-2 rounded-lg border-x11 focus:border-crayola focus:shadow-input placeholder-x11')
-    FormKit(type="password" label='Password' name='password' placeholder='Nhập mật khẩu của bạn' validation="required|length:1" input-class='transition-all border-2 rounded-lg border-x11 focus:border-crayola focus:shadow-input placeholder-x11')
+    FormKit(type='text' label='Email' name="email" placeholder='Nhập email của bạn' validation="required|email" input-class='transition-all border rounded-lg border-x11 focus:border-crayola focus:border-2 focus:shadow-input placeholder-x11')
+    FormKit(type="password" label='Password' name='password' placeholder='Nhập mật khẩu của bạn' validation="required|length:1" input-class='transition-all border rounded-lg border-x11 focus:border-crayola focus:border-2 focus:shadow-input placeholder-x11')
     .flex(class='flex-row-reverse')
       .forgot-pass.font-medium.capitalize.cursor-pointer.select-none.text-crayola(class='hover:opacity-90') Forget Password?
     FormKit(type="submit" name='login' input-class='w-full') Login
@@ -11,8 +11,6 @@ FormKit(type='form' v-model:model-value="loginData" name='login_form' id='login_
 </template>
 
 <script setup lang="ts">
-
-
 const loginData = reactive({
   email: '',
   password: ''
