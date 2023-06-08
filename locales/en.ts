3d9d8e381ad9bb2276b20af38ab535353eb63a48
kienkhang <trankienkhang2210@gmail.com>
2023-06-08 15:03:20 +0700

    feat: config language for vi and en

--- locales/en.ts
+++ locales/en.ts
@@ -1,4 +1,23 @@
 export default {
-  login: 'Login',
-  signup: 'Signup'
+  form: {
+    login_form: {
+      login: 'Login',
+      email_placeholder: 'Enter your email',
+      password: 'Password',
+      password_placeholder: 'Enter your password',
+      forgot: 'Forget Password ?',
+    },
+    signup_form: {
+      signup: 'Signup',
+      email_placeholder: 'Enter your email',
+      password: 'Password',
+      password_placeholder: 'Enter your password',
+      first_name: 'First name',
+      first_name_placeholder: 'Enter your first name',
+      last_name: 'Last name',
+      last_name_placeholder: 'Enter your last name',
+      confirm_password: 'Confirm password',
+      confirm_password_placeholder: 'Confirm your password'
+    }
+  }
 }
--- locales/vi.ts
+++ locales/vi.ts
@@ -1,4 +1,23 @@
 export default {
-  login: 'Đăng nhập',
-  signup: 'Đăng ký'
+  form: {
+    login_form: {
+      login: 'Đăng nhập',
+      email_placeholder: 'Nhập email của bạn',
+      password: 'Mật khẩu',
+      password_placeholder: 'Nhập mật khẩu của bạn',
+      forgot: 'Quên mật khẩu ?',
+    },
+    signup_form: {
+      signup: 'Đăng ký',
+      email_placeholder: 'Nhập email của bạn',
+      password: 'Mật khẩu',
+      password_placeholder: 'Nhập mật khẩu của bạn',
+      first_name: 'Họ',
+      first_name_placeholder: 'Nhập họ của bạn',
+      last_name: 'Tên',
+      last_name_placeholder: 'Nhập tên của bạn',
+      confirm_password: 'Xác nhận mật khẩu',
+      confirm_password_placeholder: 'Xác nhận lại mật khẩu của bạn'
+    }
+  }
 }
