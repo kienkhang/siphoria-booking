1f642ba50785324fb385b0e8390d9e1894ab2a22
KienKhang <trankienkhang2210@gmail.com>
2023-06-08 19:43:11 +0700

    feat: remove log

--- components/auth/LoginSocial.vue
+++ components/auth/LoginSocial.vue
@@ -1,15 +1,13 @@
 <template lang="pug">
 .grid.grid-cols-2.gap-2.px-4
-  BaseButton.justify-center.border.border-gray-400.py-2(@click='loginGG()')
+  BaseButton.justify-center.border.border-gray-400.py-2(@click='redirectGoogleUrl()')
     .i-custom-google.w-4.h-4.mr-2.flex-shrink-0
     span.font-medium.text-gray-600.text-sm Google
-  BaseButton.justify-center.border.border-gray-400.py-2(@click='loginFB()')
+  BaseButton.justify-center.border.border-gray-400.py-2(@click='redirectFacebookUrl()')
     .i-custom-facebook.w-4.h-4.mr-2.flex-shrink-0
     span.font-medium.text-gray-600.text-sm Facebook
 </template>
 
-<script setup lang="ts">
-import { redirectGoogleUrl as loginGG, redirectFacebookUrl as loginFB } from '@/utils'
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
