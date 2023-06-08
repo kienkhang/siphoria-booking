0e05cc5ca4109672bec21a9b890a9580426b8a26
kienkhang <trankienkhang2210@gmail.com>
2023-06-08 17:29:24 +0700

    feat: update color for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,18 @@ const formKitTailwind = require('@formkit/themes/tailwindcss')
 export default {
   // add the formkit.config.js file
   content: ['./src/**/*.{html,vue,js}', './formkit.config.ts'],
+  theme: {
+    extend: {
+      colors: {
+        crayola: '#1BBA85',
+        vivid: '#FF9900',
+        x11: '#A9A9A9',
+        cultured: '#F3F5F6'
+      },
+      boxShadow: {
+        input: '0px 4px 4px rgba(0, 0, 0, 0.25)'
+      }
+    }
+  },
   plugins: [formKitTailwind]
 }
