dd5cf5aaa89eda8f5486bc863f7fd4037a3d8ecc
kienkhang <trankienkhang2210@gmail.com>
2023-06-09 17:25:37 +0700

    feat: update app icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,8 +81,8 @@ export default defineNuxtConfig({
       viewport: 'width=device-width,initial-scale=1',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }
+        { rel: 'icon', type: 'image/svg+xml', href: '/siphoria.svg' },
+        { rel: 'apple-touch-icon', href: '/siphoria-192x192.png' }
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
