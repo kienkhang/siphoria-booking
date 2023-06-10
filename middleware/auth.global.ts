d0760716d4702f36dec25b67f71ac837a375c985
KienKhang <trankienkhang2210@gmail.com>
2023-06-10 17:14:32 +0700

    feat: naviagtion guard

---
+++ middleware/auth.global.ts
@@ -0,0 +1,20 @@
+import { useAccountStore } from '~/stores/account'
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  const { getMe } = useAuth()
+  const { executeAPI: fetchAccount } = getMe()
+  const { isLoggedIn } = useAuthStorage()
+  const { account } = storeToRefs(useAccountStore())
+
+  // If is logged in -> fetch account
+  if (isLoggedIn()) {
+    if (!account.value) {
+      fetchAccount()
+      return
+    }
+  }
+  // If isn't logged in and from is not account guard -> navigato('/')
+  else if (!isLoggedIn() && from.fullPath.includes('account')) {
+    return navigateTo('/')
+  }
+})
