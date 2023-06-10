ab95f69334f34b2bed168706721b226fd5683f71
KienKhang <trankienkhang2210@gmail.com>
2023-06-11 03:00:51 +0700

    feat: update max-with for name

--- components/auth/ProfileDropdown.vue
+++ components/auth/ProfileDropdown.vue
@@ -2,7 +2,7 @@
 UPopover(mode='click' class='h-8' :close-delay='50')
   .flex.items-center.rounded-2xl.py-1.pl-1.pr-4(class='border-2 border-white focus:outline-none')
     img.rounded-full.w-6.h-6.border.border-white.bg-cover.flex-shrink-0(src='https://sm.ign.com/ign_ap/cover/a/avatar-gen/avatar-generations_hugw.jpg' loading='lazy')
-    .ml-2.text-white.overflow-hidden.text-ellipsis.font-semibold.w-32 {{account?.full_name}}
+    .ml-2.text-white.overflow-hidden.text-ellipsis.font-semibold.w-max(class='max-w-[150px]') {{account?.full_name}}
   template(#panel='{ close }')
       .flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100')
         span Tài khoản của tôi
