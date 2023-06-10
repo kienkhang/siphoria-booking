e5acadf6d3f67598bd2b714e58db0ae204ac02ca
KienKhang <trankienkhang2210@gmail.com>
2023-06-10 17:29:25 +0700

    feat: render account

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,15 +1,23 @@
 <template lang="pug">
 div(class='bg-crayola')
   .max-w-7xl.mx-auto.px-4(class='sm:px-6 lg:px-8')
-    .flex.justify-between.items-center(class='h-16')
+    .flex.items-center.justify-between(class='h-16')
       div(class='w-48 h-12 cursor-pointer i-custom-logo-text-nocolor')
       .flex.items-center.gap-4
         Icon.cursor-pointer(name='mdi:cart-outline' color='white' size='24')
         //- div(class='flex-shrink-0 w-8 h-8 text-white cursor-pointer select-none i-mdi:cart-outline')
         SharedLanguageSelect
-        AuthButton
+        AuthButton(v-if='!isLogin')
+        AuthProfileDropdown(:account='account' v-else)
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAccountStore } from '~/stores/account'
+
+// Check account exist
+const { account } = storeToRefs(useAccountStore())
+const { isLoggedIn } = useAuthStorage()
+const isLogin = computed(() => isLoggedIn() && account.value)
+</script>
 
 <style scoped></style>
