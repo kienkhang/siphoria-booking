0af1ff35f8c809c62e262a882adedecb0fb339b9
KienKhang <trankienkhang2210@gmail.com>
2023-06-12 18:33:24 +0700

    feat: dont ui info

---
+++ components/account/Info.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.w-full
+  .w-full.h-288px.relative(:style='{background: `url("${bgImg}") no-repeat center/cover`}')
+    //- nuxt-img(
+    //-   src='/saigon_resize.jpg' 
+    //-   loading='lazy' 
+    //-   preload
+    //-   class='w-full mx-auto overflow-hidden bg-cover h-288px' 
+    //-   format='jpg'
+    //- )
+    .max-w-7xl.absolute(class='w-full -translate-x-1/2 -translate-y-1/2 left-1/2 -bottom-2/4')
+      div(class='w-1/4')
+        SharedAvatar.mx-auto
+  .max-w-7xl.w-full.flex.justify-center.mx-auto
+    div(class='w-1/4')
+    .mt-5.mb-9(class='w-3/4')
+      span.text-4xl.font-bold Kiện Khang
+      .mt-2
+        span.flex.items-center.select-none 
+          span Hạng thành viên &nbsp
+          span.text-vivid.font-bold.cursor-pointer  Bạch Kim
+    
+  
+</template>
+
+<script setup lang="ts">
+import { useAccountStore } from '~/stores/account'
+
+import bgImg from '@/assets/images/saigon_resize.jpg'
+// destruct account store
+const { account } = storeToRefs(useAccountStore())
+</script>
+
+<style scoped></style>
