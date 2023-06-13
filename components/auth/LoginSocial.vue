44de733e2591e429bf1a45d2c99784092ba09cfd
kienkhang <trankienkhang2210@gmail.com>
2023-06-13 11:59:57 +0700

    feat: add hover

--- components/auth/LoginSocial.vue
+++ components/auth/LoginSocial.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .grid.grid-cols-1.gap-2.px-4
-  BaseButton.justify-center.border.border-gray-400.py-2(@click='redirectGoogleUrl()')
-    .i-custom-google.w-4.h-4.mr-2.flex-shrink-0
+  BaseButton.justify-center.border.border-gray-400.py-2(@click='redirectGoogleUrl()' class='hover:bg-ghost-white')
+    .w-4.h-4.mr-2.flex-shrink-0(class='i-custom-google?bg')
     span.font-medium.text-gray-600.text-sm Google
   //- BaseButton.justify-center.border.border-gray-400.py-2(@click='redirectFacebookUrl()')
   //-   .i-custom-facebook.w-4.h-4.mr-2.flex-shrink-0
