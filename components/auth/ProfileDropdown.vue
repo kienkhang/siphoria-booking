f1427bb69a94461df880f80ed59a4e6f7bc847e1
kienkhang <trankienkhang2210@gmail.com>
2023-06-14 17:10:44 +0700

    feat: remove old ui and change data show

--- components/auth/ProfileDropdown.vue
+++ components/auth/ProfileDropdown.vue
@@ -1,20 +1,4 @@
 <template lang="pug">
-//- UPopover(mode='click' class='h-8' :close-delay='50')
-//-   .flex.items-center.rounded-2xl.py-1.pl-1.pr-4(class='border-2 border-white focus:outline-none')
-//-     img.rounded-full.w-6.h-6.border.border-white.bg-cover.flex-shrink-0(:src='account?.avatar' loading='lazy')
-//-     .ml-2.text-white.overflow-hidden.text-ellipsis.font-semibold.w-max(class='max-w-[150px]') {{account?.full_name}}
-//-   template(#panel='{ close }')
-//-     .flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100')
-//-       span Tài khoản của tôi
-//-     .flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100')
-//-       span Đặt chỗ của tôi
-//-     .flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100')
-//-       span Lịch sử đặt phòng
-//-     .flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100')
-//-       span Đổi mật khẩu
-//-     .flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100' @click='logout')
-//-       span Đăng xuất 
-
 NPopover.max-w-192px.w-max(
   trigger='click'
   style='background-color: white; padding: 0px; height: 100%; border-radius: 10px'
@@ -28,7 +12,7 @@ NPopover.max-w-192px.w-max(
   template(#trigger)
     .flex.items-center.rounded-2xl.py-1.pl-1.pr-4(class='border-2 border-white cursor-pointer select-none focus:outline-none')
       img.rounded-full.w-6.h-6.border.border-white.bg-cover.flex-shrink-0(:src='account?.avatar' loading='lazy')
-      .ml-2.text-white.overflow-hidden.text-ellipsis.font-semibold.w-max(class='max-w-[150px]') {{account?.full_name}}
+      .ml-2.text-white.overflow-hidden.text-ellipsis.font-semibold.w-max(class='max-w-[150px]') {{account?.first_name}} {{ account?.last_name }}
   .w-full.select-none.flex.flex-col.rounded-10px.overflow-hidden(@click='updateShow(false)')  
     nuxt-link.flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100' to='/account')
       Icon.flex-shrink-0(name='mdi:account' size='16')
