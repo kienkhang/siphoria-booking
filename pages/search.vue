648d37855f4db51d1ad981c3a8b613dae9fa41c3
kienkhang <trankienkhang2210@gmail.com>
2023-06-15 17:29:57 +0700

    feat: using home layout

---
+++ pages/search.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+.flex.items-center.flex-wrap.gap-4
+  LazySharedHotelCard
+  LazySharedHotelCard
+  
+
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home',
+  title: 'Siphoria | Search Hotel'
+})
+</script>
+
+<style scoped></style>
