8831d99302e453fb9143e9245a9f9e6ec6fb6bfc
KienKhang <trankienkhang2210@gmail.com>
2023-06-18 17:36:43 +0700

    fix: show icon with background color

--- components/auth/Redirect.vue
+++ components/auth/Redirect.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .w-full.h-full.flex.items-center.justify-center
   .flex.flex-col.items-center.gap-3
-    div(class='i-custom-logo-origin h-28 w-28')
+    div(class='i-custom-logo-origin?bg h-28 w-28')
     div(class='w-10 h-10 i-eos-icons:bubble-loading')
     .flex.items-end 
       span.text-base Đang chuyển hướng
