8cd66baea30873d91a18a5e574a6244c179b2885
KienKhang <trankienkhang2210@gmail.com>
2023-06-29 14:35:50 +0700

    feat: add filter search hotel

---
+++ components/search/SearchFilter.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.flex.justify-end.items-center.gap-4
+  //- 
+  SearchFilterPrice
+  SearchFilterRate
+
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ components/search/SearchFilterPrice.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+NPopover(
+  trigger='click'
+  :show-arrow='false'
+  style='margin: 0; margin-bottom: 16px; padding: 0; border-radius: 16px;'
+  placement='bottom-start'
+  v-model:show='show'
+)
+  template(#trigger)
+    .flex.items-center.rounded-lg.px-4.py-2.border.cursor-pointer.select-none
+      span.mr-3 Price
+      div(:class='["w-4 h-4 i-custom-chevron-down transition-all", {"rotate-180": show}]')
+  .rounded-16px.bg-white.w-320px.overflow-y-auto.hide-scroll.mt-2.flex.flex-col.p-3.gap-8.shadow-search(v-auto-animate)
+    | Filter Price
+
+
+</template>
+
+<script setup lang="ts">
+const show = ref(false)
+</script>
+
+<style scoped></style>
