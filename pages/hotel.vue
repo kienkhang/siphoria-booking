cda74da7b92c9b14bd1b5006674c5137eeccca57
KienKhang <trankienkhang2210@gmail.com>
2023-06-29 14:36:17 +0700

    feat: add hotel page

---
+++ pages/hotel.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+NuxtPage
+</template>
+
+<script setup lang="ts">
+import useHotel from '~/composables/useHotel'
+definePageMeta({
+  layout: 'home',
+  title: 'Siphoria | Search Hotel'
+})
+</script>
+
+<style scoped></style>
---
+++ pages/hotel/[id].vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const params = computed(() => route.params)
+console.log('😃😦😧 ~ params:', params.value)
+const query = computed(() => route.query)
+console.log('😃😦😧 ~ query:', query.value)
+</script>
+
+<style scoped></style>
