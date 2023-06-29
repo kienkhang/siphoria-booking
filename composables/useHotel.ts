dc60227a0bdcc2b1da5fa2fd4f5ea88c61b036e6
KienKhang <trankienkhang2210@gmail.com>
2023-06-29 22:00:03 +0700

    feat: get detail hotel

--- composables/useHotel.ts
+++ composables/useHotel.ts
@@ -1,6 +1,26 @@
 import { hotelApi } from '~/apis/hotel'
 
 function useHotel() {
+  // Get Hotel Details API
+  function getDetails(params: Ref<{} | any>, hotelId: Ref<string>) {
+    const usedGet = hotelApi.details(params.value, hotelId.value)
+    // destructuring search hotel
+    const { execute } = usedGet
+    // custom execute api
+    const executeApi = async () => {
+      try {
+        await execute({ params: params.value })
+      } catch (e: any) {
+        throw new Error(e)
+      }
+    }
+    return {
+      ...usedGet,
+      executeApi
+    }
+  }
+
+  // Search Hotel API
   function searchHotel(params: Ref<any | {}>) {
     const usedSearch = hotelApi.search(params.value)
     // destructuring search hotel
@@ -20,7 +40,8 @@ function useHotel() {
   }
 
   return {
-    searchHotel
+    searchHotel,
+    getDetails
   }
 }
 
