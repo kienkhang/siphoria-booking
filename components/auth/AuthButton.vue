687631e712e06a0f8e13f2984f5574e8ffd3a967
KienKhang <trankienkhang2210@gmail.com>
2023-06-29 14:36:42 +0700

    feat: border radius for button

--- components/auth/AuthButton.vue
+++ components/auth/AuthButton.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-BaseButton.px-4.py-2.border.border-white.text-white(class='hover:bg-white hover:text-green-500' )
+BaseButton.px-4.py-2.border.border-white.text-white.rounded-lg(class='hover:bg-white hover:text-green-500' )
   span.font-medium(@click='show = true') {{$t("form.login_form.login")}} / {{$t("form.signup_form.signup")}}
 AuthModal(v-model:show='show')
 </template>
