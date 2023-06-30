017fc2ef76d70bb18bd6350b37d2673e963e1f7e
KienKhang <trankienkhang2210@gmail.com>
2023-07-01 03:06:08 +0700

    feat: translate modal

--- components/auth/AuthModal.vue
+++ components/auth/AuthModal.vue
@@ -4,9 +4,9 @@ BaseModal(:show='show' @mask-click='closeModal')
     //- Login Tab 
     .tabs-list.w-full.flex.items-center.justify-center
       .tab-items.flex.items-center.justify-center.h-9.cursor-pointer(class='w-40' :class='{"shadow-[0px_2px_#FF9900]":tab==="login"}' @click='changeTab("login")')
-        span(:class='[{"font-semibold text-vivid":tab==="login"},{"text-gray":tab!=="login"}]') Login
+        span(:class='[{"font-semibold text-vivid":tab==="login"},{"text-gray":tab!=="login"}]') {{$t('form.login_form.login')}}
       .tab-items.flex.items-center.justify-center.h-9.cursor-pointer(class='w-40' :class='{"shadow-[0px_2px_#FF9900]":tab==="signup"}' @click='changeTab("signup")')
-        span(:class='[{"font-semibold text-vivid":tab==="signup"},{"text-gray":tab!=="signup"}]') Signup
+        span(:class='[{"font-semibold text-vivid":tab==="signup"},{"text-gray":tab!=="signup"}]') {{$t('form.signup_form.signup')}}
     .tab-panels.mt-4(v-auto-animate)
       LoginSocial
       .panel-item(v-if='tab==="login"')
