a6cf55ea919b18ba46038d9b948fba83bbe3247b
KienKhang <trankienkhang2210@gmail.com>
2023-07-01 03:06:15 +0700

    feat: add rounded

--- components/auth/LoginSocial.vue
+++ components/auth/LoginSocial.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .grid.grid-cols-1.gap-2.px-4
-  BaseButton.justify-center.border.border-gray-400.py-2(@click='redirectGoogleUrl()' class='hover:bg-ghost-white')
+  BaseButton.justify-center.border.border-gray-400.rounded-lg.py-2(@click='redirectGoogleUrl()' class='hover:bg-ghost-white')
     .w-4.h-4.mr-2.flex-shrink-0(class='i-custom-google?bg')
     span.font-medium.text-gray-600.text-sm Google
   //- BaseButton.justify-center.border.border-gray-400.py-2(@click='redirectFacebookUrl()')
