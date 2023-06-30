e50ea321498c319f4065ca7b1781dd7092cfaf2c
KienKhang <trankienkhang2210@gmail.com>
2023-07-01 00:23:25 +0700

    feat: remove unuse function and update width

--- components/shared/GalleryCarousel.vue
+++ components/shared/GalleryCarousel.vue
@@ -2,8 +2,9 @@
 NModal(
   v-model:show='show'
   :mask-closable='true'
+
 )
-  .flex.items-center.justify-between.w-full.relative.px-4
+  .flex.items-center.justify-between.w-screen.h-screen.relative.px-4
     //- Left arrow 
     .rounded-full.w-10.h-10.bg-white.flex.items-center.justify-center.flex-shrink-0.cursor-pointer.select-none(
       :class='["hover:opacity-90", {"cursor-not-allowed bg-x11":!isPrev}]'
@@ -12,9 +13,8 @@ NModal(
       .w-6.h-6(class='rotate-90 i-custom-chevron-down')
     //- Image
     div
-      n-carousel.max-w-747px.w-max(
+      n-carousel.max-w-800px.w-max(
         v-model:current-index='index'
-        draggable 
         :show-dots='false'
       )
         n-carousel-item(
@@ -35,6 +35,8 @@ NModal(
       @click='next()'
     )
       .w-6.h-6(class='-rotate-90 i-custom-chevron-down')
+    .absolute.top-6.right-5(@click='show = false')
+      div(class='w-6 h-6 text-white cursor-pointer i-mdi:close hover:opacity-90')
     teleport(to='body')
       .fixed.inset-0(class='z-50 bg-black/25 backdrop-blur-1px mask-overlay dark:bg-white/50' v-if='show')
 </template>
@@ -45,11 +47,8 @@ const props = defineProps<{
 }>()
 const show = defineModel<boolean>('show')
 
+// Handle click next and prev image
 const index = ref(0)
-
-function update(currentIndex: number) {
-  index.value = currentIndex
-}
 
 const isNext = computed(() => index.value < props.photos.length)
 const isPrev = computed(() => index.value > 0)
