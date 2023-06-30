5f9b676114ec0652d3f4dc8ace42a13838f6de9a
KienKhang <trankienkhang2210@gmail.com>
2023-07-01 00:22:37 +0700

    feat: done layout hotel facilities

---
+++ components/hotel/Facility.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.h-72px.w-40.flex.flex-col.items-center
+  div(class='w-8 h-8 text-black i-custom-plane?bg' )
+  span.mt-4 Airport pick-up
+
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ components/hotel/HotelFacilities.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+.flex.flex-col.gap-6
+  span.text-xl.font-medium What you have in this property
+  .grid.grid-cols-5.justify-items-center.gap-6
+    HotelFacility(v-for='i in 9')
+    BaseButton(class='justify-center w-full p-2 bg-green-300 border rounded-2xl') +20 More
+
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
