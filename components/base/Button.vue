c279c93232f40eeb72ba42354d82c09c76b35fad
KienKhang <trankienkhang2210@gmail.com>
2023-07-04 02:19:52 +0700

    feat: default medium font weight

--- components/base/Button.vue
+++ components/base/Button.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-button.flex-inline.items-center.transition-all(class='active:focus:scale-95 hover:bg-opacity-90' @click='$emit("click")' )
+button.flex-inline.items-center.transition-all.font-medium(class='active:focus:scale-95 hover:bg-opacity-90' @click='$emit("click")' )
   slot
 </template>
 
