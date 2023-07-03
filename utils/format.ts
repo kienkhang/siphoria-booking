8d287d806b62104b15f3298d42e3005bb3160276
KienKhang <trankienkhang2210@gmail.com>
2023-07-04 02:09:50 +0700

    feat: update format currency

--- utils/format.ts
+++ utils/format.ts
@@ -1,7 +1,6 @@
-const VND = new Intl.NumberFormat('vi-VN', {
-  style: 'currency',
-  currency: 'VND'
-})
+const VND = (moneyString: string) => {
+  return moneyString.replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+}
 
 const Image2Array = (text: string) => {
   if (!text) return []
