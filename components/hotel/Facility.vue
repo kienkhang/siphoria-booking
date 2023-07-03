4bb3a117b7a093c230ca0fc205711dfff2e0f6de
KienKhang <trankienkhang2210@gmail.com>
2023-07-04 02:50:15 +0700

    fix: replace character

--- components/hotel/Facility.vue
+++ components/hotel/Facility.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
-.h-72px.w-40.flex.flex-col.items-center
+.h-72px.w-40.flex.flex-col.items-center.select-none
   //- div(class=' i-custom-bar?bg' :class='iconClass')
   nuxt-icon(
     :name='name'
     class='w-8 h-8'
   )
-  span.mt-4.capitalize {{ name }}
+  span.mt-4.capitalize {{ name.replaceAll('_',' ') }}
 
 </template>
 
