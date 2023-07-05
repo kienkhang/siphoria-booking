9b445db8d1afa05231fcf1195ecf4d89581687df
KienKhang <trankienkhang2210@gmail.com>
2023-07-05 16:49:59 +0700

    feat: open auth modal default

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,24 +1,25 @@
 import { useAccountStore } from '~/stores/account'
+import { useAuthStore } from '~/stores/auth'
 
 export default defineNuxtRouteMiddleware((to, from) => {
   const { getMe } = useAuth()
   const { executeAPI: fetchAccount } = getMe()
   const { isLoggedIn } = useAuthStorage()
-  const { account } = storeToRefs(useAccountStore())
-
+  const { isAuthorized } = storeToRefs(useAuthStore())
   // If is logged in -> fetch account
+  const { openAuthModal } = useAuthStore()
   if (isLoggedIn()) {
-    if (!account.value) {
+    if (!isAuthorized.value) {
       fetchAccount()
       return
     }
   }
   // If isn't logged in and from is not account guard -> navigato('/')
   else if (
-    !isLoggedIn() &&
-    !account.value &&
+    !isAuthorized.value &&
     ['cart', 'account', 'checkout'].some((path) => to.fullPath.includes(path))
   ) {
+    openAuthModal()
     return navigateTo('/')
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,16 @@ HomeHotelType.mt-16
 </template>
 
 <script setup lang="ts">
+import { useAuthStore } from '~/stores/auth'
+
 definePageMeta({
   layout: 'home',
   title: 'My Home page'
 })
+
+const { openAuthModal } = useAuthStore()
+
+openAuthModal()
 </script>
 
 <style scoped></style>
