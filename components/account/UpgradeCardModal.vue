6e2a2b96ab41cb3b826dc025944e403f374eaef2
KienKhang <trankienkhang2210@gmail.com>
2023-07-11 18:25:46 +0700

    feat: init upgrade member modal

---
+++ components/account/UpgradeCardModal.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+NModal(
+  :show='show' 
+  @mask-click='closeModal'
+)
+  .bg-white.max-w-2xl.w-full.rounded-xl.h-600px
+</template>
+
+<script setup lang="ts">
+// Define Props
+const props = defineProps<{
+  show: boolean
+}>()
+// Define Emits
+const emit = defineEmits<{
+  (e: 'update:show', value: boolean): void
+}>()
+
+function closeModal() {
+  emit('update:show', false)
+}
+</script>
+
+<style scoped></style>
--- pages/account/index.vue
+++ pages/account/index.vue
@@ -4,8 +4,11 @@
     //- Title & Upgrade button
     .flex.justify-between.items-center
       span.text-4xl.font-bold Membership Card
-      BaseButton.px-4.py-2.border.border-vivid.text-vivid.rounded-lg(class='hover:bg-vivid hover:text-white')
+      BaseButton.px-4.py-2.border.border-vivid.text-vivid.rounded-lg(class='hover:bg-vivid hover:text-white' @click='openUpgrade')
         span.font-medium Upgrade
+      AccountUpgradeCardModal(
+        v-model:show='showUpgrade'
+      )
     //- membership card
     .flex.items-center.gap-6.mt-6
       AccountMemberCard.flex-1
@@ -27,6 +30,12 @@
 <script setup lang="ts">
 // Update
 const showUpdate = ref(false)
+// show upgrade card modal
+const showUpgrade = ref(false)
+
+function openUpgrade() {
+  showUpgrade.value = true
+}
 </script>
 
 <style scoped></style>
