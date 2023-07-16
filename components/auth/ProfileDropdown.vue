973c5f44323fbdd48ba12d3659f2a7551a376ad5
KienKhang <trankienkhang2210@gmail.com>
2023-07-17 00:04:52 +0700

    feat: translate dropdown

--- components/auth/ProfileDropdown.vue
+++ components/auth/ProfileDropdown.vue
@@ -16,20 +16,20 @@ NPopover.max-w-192px.w-max(
   .w-full.select-none.flex.flex-col.rounded-10px.overflow-hidden(@click='updateShow(false)')  
     nuxt-link.flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100' to='/account')
       Icon.flex-shrink-0(name='mdi:account' size='16')
-      span Tài khoản của tôi
+      span {{ $t(`account_page.navbar.profile`) }}
     nuxt-link.flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100' to='/account/wallet')
       Icon.flex-shrink-0(name='mdi:wallet-outline' size='16')
-      span Ví của tôi
+      span {{ $t(`account_page.navbar.my_wallet`) }}
     nuxt-link.flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100' to='/account/booking')
       Icon.flex-shrink-0(name='mdi:calendar' size='16')
-      span Đặt chỗ của tôi
+      span {{ $t(`account_page.navbar.my_bookings`) }}
     nuxt-link.flex.items-center.gap-3.py-2.px-4(class='cursor-pointer hover:bg-gray-100' to='/account/history')
       Icon.flex-shrink-0(name='mdi:calendar-outline' size='16')
-      span Lịch sử đặt phòng
+      span {{ $t(`account_page.navbar.my_history`) }}
     hr
     .flex.items-center.gap-3.py-2.px-4.text-red-500(class='cursor-pointer hover:bg-gray-100' @click='doLogout()')
       Icon.flex-shrink-0(name='mdi:logout' size='16')
-      span Đăng xuất 
+      span {{ $t(`the_header.logout`) }}
 
 
 </template>
